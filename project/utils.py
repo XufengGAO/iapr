cf77ab14c6e8d205c8ed3133748e9b89df26b224
hibetterheyj <he-yujie@outlook.com>
2022-06-03 06:28:41 +0200

    update project.ipynb

--- project/utils.py
+++ project/utils.py
@@ -5,18 +5,26 @@ from treys import Card
 import pandas as pd
 from termcolor import colored
 
-def print_color(my_string , color = 'red'):
+
+def print_color(my_string, color='red'):
     if color == "black" or color == "white":
         if color == "black":
             color = "grey"
-        print(colored(my_string, color, attrs=['bold', 'reverse' ]) , end='')
+        print(colored(my_string, color, attrs=['bold', 'reverse']), end='')
     else:
-        print(colored(my_string, color, attrs=['bold', ]) , end='')
+        print(
+            colored(
+                my_string,
+                color,
+                attrs=[
+                    'bold',
+                ],
+            ),
+            end='',
+        )
 
 
-
-
-def eval_game(game_dict, true_game : list , verbose = True):
+def eval_game(game_dict, true_game: list, verbose=True):
     """
     Scores the predicted game labels with the true game labels
     Parameters
@@ -30,60 +38,71 @@ def eval_game(game_dict, true_game : list , verbose = True):
     errors : list of bool with the errors in the three task of number, suit and chip identification
     """
 
+    # list to dict, with THOSE headers
+    header_dict = [
+        'T1',
+        'T2',
+        'T3',
+        'T4',
+        'T5',
+        'P11',
+        'P12',
+        'P21',
+        'P22',
+        'P31',
+        'P32',
+        'P41',
+        'P42',
+        'CR',
+        'CG',
+        'CB',
+        'CK',
+        'CW',
+    ]
+    game = [game_dict[key] for key in header_dict]
 
-    #list to dict, with THOSE headers
-    header_dict = ['T1', 'T2', 'T3' , 'T4' ,'T5' , 'P11' , 'P12' , 'P21' , 'P22' , 'P31' , 'P32' , 'P41', 'P42' , 'CR' , 'CG' , 'CB' , 'CK' , 'CW']
-    game = [ game_dict[key] for key in header_dict]
+    if verbose:
+        print("Estimated game")
+        print(game)
+        print("True game")
+        print(true_game)
+        print("\n")
 
-    print("Estimated game")
-    print(game)
-    print("True game")
-    print(true_game)
-    print("\n")
-    
-    def parse_game(game : list):
+    def parse_game(game: list):
         cards = game[:13]
         chips = game[13:]
 
         card_numbers = [card[:-1] for card in cards]
-        card_suits   = [card[-1:].lower() for card in cards]
-        chips        = [int(chip) for chip in chips]
-
+        card_suits = [card[-1:].lower() for card in cards]
+        chips = [int(chip) for chip in chips]
 
         return np.array(card_numbers), np.array(card_suits), np.array(chips)
-    
-    
-    numbers     ,   suits   , chips      = parse_game(game)
+
+    numbers, suits, chips = parse_game(game)
     true_numbers, true_suits, true_chips = parse_game(true_game)
     weight_chip = np.sum(true_chips) + 1
-    weight_chip = np.minimum(1/weight_chip , .2)
+    weight_chip = np.minimum(1 / weight_chip, 0.2)
 
-    errors = list(1 - np.logical_and(numbers== true_numbers , suits == true_suits))
+    errors = list(1 - np.logical_and(numbers == true_numbers, suits == true_suits))
     errors += list(chips - true_chips)
-    
-    
 
-    score_num   = np.mean(numbers == true_numbers)
-    score_suits = np.mean(suits == true_suits) 
-    score_chips = np.sum(np.abs(chips - true_chips)*weight_chip)
-    score_chips = np.maximum(0 , 1- score_chips)
-    final_score = (score_num + score_suits + score_chips)/3
-                                
+    score_num = np.mean(numbers == true_numbers)
+    score_suits = np.mean(suits == true_suits)
+    score_chips = np.sum(np.abs(chips - true_chips) * weight_chip)
+    score_chips = np.maximum(0, 1 - score_chips)
+    final_score = (score_num + score_suits + score_chips) / 3
+
     if verbose:
         print("             \tscores")
-        print(f"Card number \t{round(score_num,2)} %" )
-        print(f"Card suit   \t{round(score_suits,2)} %" )
-        print(f"Chips       \t{round(score_chips,2)} %" )
+        print(f"Card number \t{round(score_num,2)} %")
+        print(f"Card suit   \t{round(score_suits,2)} %")
+        print(f"Chips       \t{round(score_chips,2)} %")
         print("\n")
         print(f"FINAL score \t{round(final_score,2)} %")
-    return final_score ,  errors 
-
-    
+    return final_score, errors
 
 
-
-
-def print_game(game_dict , true_game ):
+def print_game(game_dict, true_game):
     """
     Pretty prints the predicted game labels with the true game labels
     Parameters
@@ -91,14 +110,33 @@ def print_game(game_dict , true_game ):
     game_dict : dict with the cards and chips detected for the game_id
     true_game : list of the cards and chips in the true game
     ----------
-    Returns 
+    Returns
     None
     """
-    #list to dict, with THOSE headers
-    header_dict = ['T1', 'T2', 'T3' , 'T4' ,'T5' , 'P11' , 'P12' , 'P21' , 'P22' , 'P31' , 'P32' , 'P41', 'P42' , 'CR' , 'CG' , 'CB' , 'CK' , 'CW']
-    game = [ game_dict[key] for key in header_dict]
-    
-    score, errors_game =  eval_game(game_dict, true_game , verbose = False)
+    # list to dict, with THOSE headers
+    header_dict = [
+        'T1',
+        'T2',
+        'T3',
+        'T4',
+        'T5',
+        'P11',
+        'P12',
+        'P21',
+        'P22',
+        'P31',
+        'P32',
+        'P41',
+        'P42',
+        'CR',
+        'CG',
+        'CB',
+        'CK',
+        'CW',
+    ]
+    game = [game_dict[key] for key in header_dict]
+
+    score, errors_game = eval_game(game_dict, true_game, verbose=False)
 
     def handle_empty(card):
         if card == '0' or card == 0:
@@ -108,89 +146,118 @@ def print_game(game_dict , true_game ):
 
     def parse_pretty_hand(game):
         board = game[:5]
-        all_players = [ game[5+i*2:5+(i+1)*2] for i in range(4)]
+        all_players = [game[5 + i * 2 : 5 + (i + 1) * 2] for i in range(4)]
         chips = game[13:]
-        
-        board = [  handle_empty(ci)  for ci in board]
-        hands = [ [handle_empty(ci)  for ci in pi ] for pi in all_players]
-        chips     = [int(chip) for chip in chips]
-        
-        return hands , board ,chips
+
+        board = [handle_empty(ci) for ci in board]
+        hands = [[handle_empty(ci) for ci in pi] for pi in all_players]
+        chips = [int(chip) for chip in chips]
+
+        return hands, board, chips
 
     def print_hand(my_hand):
-        #print(" --- " , my_hand)
+        # print(" --- " , my_hand)
         if my_hand[0] != None:
             print(Card.print_pretty_cards(my_hand))
-        else :
+        else:
             print(my_hand)
-    
-    hands_found, board_found ,chips_found = parse_pretty_hand(game)
-    hands_true , board_true  ,chips_true  = parse_pretty_hand(true_game)
-    
+
+    hands_found, board_found, chips_found = parse_pretty_hand(game)
+    hands_true, board_true, chips_true = parse_pretty_hand(true_game)
 
     #### Printing game begins
-    
-    #printing table cards
-    print("__"*20)
 
-    print("Table " ,end = '')
+    # printing table cards
+    print("__" * 20)
+
+    print("Table ", end='')
     errors_table = errors_game[0:5]
-    if np.sum(errors_table )!=0:
+    if np.sum(errors_table) != 0:
 
-        print_color("mistakes in cards : "  , 'red')
-        print(list(np.where(errors_table)[0] ))
+        print_color("mistakes in cards : ", 'red')
+        print(list(np.where(errors_table)[0]))
         print("True table")
-        print(Card.print_pretty_cards(board_true ))
+        print(Card.print_pretty_cards(board_true))
         print("Found table")
-        print(Card.print_pretty_cards(board_found ))
-    else :
-        print_color("found correctly"  , 'green')
+        print(Card.print_pretty_cards(board_found))
+    else:
+        print_color("found correctly", 'green')
         print("\n")
-        print(Card.print_pretty_cards(board_found ))
+        print(Card.print_pretty_cards(board_found))
     print('\n')
-        
+
     ##### Printing players cards
-    ascii_check = [ "---->" , "xxxxx"]
-    #ascii_circle = [🔴,🟢,🔵,⚫,⚪]
-    ascii_circle  =[ 0,1,2,3,4]
+    ascii_check = ["---->", "xxxxx"]
+    # ascii_circle = [🔴,🟢,🔵,⚫,⚪]
+    ascii_circle = [0, 1, 2, 3, 4]
     errors_hands = errors_game[5:-5]
     for i in range(4):
-        sum_error = np.sum(errors_hands[i*2:(i+1)*2])
-        right = ascii_check[0] if sum_error == 0 else  ascii_check[1]
-        if sum_error  ==0:
-            print_color(ascii_check[0] , 'green')
+        sum_error = np.sum(errors_hands[i * 2 : (i + 1) * 2])
+        right = ascii_check[0] if sum_error == 0 else ascii_check[1]
+        if sum_error == 0:
+            print_color(ascii_check[0], 'green')
         else:
-            print_color(ascii_check[1] , 'red')
+            print_color(ascii_check[1], 'red')
 
-        print(f"Player {i+1} " , end= '')
-        if sum_error ==0:
-            print_color("found correctly" , 'green')
+        print(f"Player {i+1} ", end='')
+        if sum_error == 0:
+            print_color("found correctly", 'green')
             print_hand(hands_true[i])
         else:
-            print_color("error" , 'red')
-            print ("\n True hand")
+            print_color("error", 'red')
+            print("\n True hand")
             print_hand(hands_true[i])
             print("Estimated hand")
-            print_hand(hands_found[i])                
+            print_hand(hands_found[i])
         print('\n')
-    
+
     #### Printing chips
-    chip_names = [ 'Red' , 'Green' , 'Blue' , 'Black' , 'White']
-    chip_color = [ 'red' , 'green' , 'blue' , 'black' , 'white']
+    chip_names = ['Red', 'Green', 'Blue', 'Black', 'White']
+    chip_color = ['red', 'green', 'blue', 'black', 'white']
     for i in range(5):
         diff = chips_found[i] - chips_true[i]
-        if diff ==0:
-            print_color(ascii_check[0]  , "green")
+        if diff == 0:
+            print_color(ascii_check[0], "green")
         else:
-            print_color(ascii_check[1]  , "red")
-        print_color(f"Chip {chip_names[i]}" , chip_color[i])
-        print(f"\tfound: {chips_found[i]}. Count error: ( {diff} )"  , end = '\n')
-            
-    print("__"*20)
-    
+            print_color(ascii_check[1], "red")
+        print_color(f"Chip {chip_names[i]}", chip_color[i])
+        print(f"\tfound: {chips_found[i]}. Count error: ( {diff} )", end='\n')
 
-    
-def eval_listof_games( results_games , game_labels ,game_id = [0,1]):
+    print("__" * 20)
+
+
+def eval_listof_games_custom(results_games, game_labels, game_id=[0, 1], verbose=False):
+    """
+    Evaluates the results of a list of game_labels
+    Parameters
+    ----------
+    results_games : list of dictionaries with the cards and chips detected for the game_id
+    game_labels   : csv file with the true game game_labels
+    game_id       : list of the game_id to Evaluates
+    verbose       : verbose flag
+    ----------
+    Returns
+    -------
+    average_score : float with the final score
+    score         : float with all scores
+    """
+    scores = []
+    for my_id in game_id:
+        if verbose:
+            print(f"Game {my_id} results")
+        game_true = game_labels.iloc[my_id].values[1:]
+        game_found = results_games[my_id]
+        score, errors = eval_game(game_found, game_true, verbose=verbose)
+        if verbose:
+            print("__" * 20)
+        scores.append(score)
+
+    average_score = np.mean(np.array(scores))
+    print("Average SCORE = ", average_score)
+    return average_score, scores
+
+
+def eval_listof_games(results_games, game_labels, game_id=[0, 1]):
     """
     Evaluates the results of a list of game_labels
     Parameters
@@ -208,15 +275,16 @@ def eval_listof_games( results_games , game_labels ,game_id = [0,1]):
         print(f"Game {my_id} results")
         game_true = game_labels.iloc[my_id].values[1:]
         game_found = results_games[my_id]
-        score , errors = eval_game( game_found ,game_true , verbose = True)
-        print("__"*20)
+        score, errors = eval_game(game_found, game_true, verbose=True)
+        print("__" * 20)
         scores.append(score)
-    
+
     average_score = np.mean(np.array(scores))
     print("Average SCORE = ", average_score)
     return average_score
 
-def debug_listof_games( results_games , game_labels ,game_id = [0,1]):
+
+def debug_listof_games(results_games, game_labels, game_id=[0, 1]):
     """
     Pretty print and detailed evaluation of the results of a dict of game_labels
     Parameters
@@ -232,11 +300,12 @@ def debug_listof_games( results_games , game_labels ,game_id = [0,1]):
         print(f"Game {my_id} results")
         game_true = game_labels.iloc[my_id].values[1:]
         game_found = results_games[my_id]
-        print_game( game_found ,game_true )
-        print("__"*20)
+        print_game(game_found, game_true)
+        print("__" * 20)
     return
 
-def save_results(results= {} , groupid=0):
+
+def save_results(results={}, groupid=0):
     """
     Save predctions
     Parameters
@@ -260,8 +329,6 @@ def load_results(file):
     file: str
         File name of the json file with the predictions
     """
-    with open(file ,  'r') as f:
+    with open(file, 'r') as f:
         data = json.load(f)
     return data
-
-
